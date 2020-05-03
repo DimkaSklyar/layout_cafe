473c3b27dac1032294b2e03a7aad4dee1604cde0
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2020-05-03 22:35:38 +0300

    v.0.1.0

--- src/js/common.js
+++ src/js/common.js
@@ -2,4 +2,90 @@ $(document).ready(function () {
 
   $('.slider').slick();
 
+  $('.testimonial-carousel').slick();
+
+  $('.carousel-pizza').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: $('.prev_pizza'),
+    nextArrow: $('.next_pizza'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  
+
+  $('.recommend-carousel').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  var mixer = mixitup('.mixitup-1');
+
+
+  $(".menu_tabs__item").click(function (e) {
+    $(".menu_tabs__item").removeClass("active");
+    $(this).addClass("active");
+    let posTabs = $(this).addClass("active").index();
+    $(".menu_tabs__content").removeClass("active");
+    $(".menu_tabs__content").eq(posTabs).addClass("active");
+  });
 });
